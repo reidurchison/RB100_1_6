cd1f2b7a5b5ed085c3615f9323ff9f27311f7c4d
Reid U <rurchison@gmail.com>
2023-02-22 16:05:00 +0000

    progress from February 21st, 2023

---
+++ 7arrays/flatten.rb
@@ -0,0 +1,9 @@
+a = [1, 2, 3, 4, [1, 2, 3,], [5, 6, 7], 8, 9]
+
+puts "#{a}"
+
+puts "#{a.flatten}"
+
+puts "#{a}"
+
+# conclusion: flatten method is not destructive
---
+++ 7arrays/sort.rb
@@ -0,0 +1,9 @@
+a = [3, 4, 5, 2, 5, 1]
+
+puts "#{a}"
+
+puts "#{a.sort}"
+
+puts "#{a}"
+
+# conclusion: sort method is not destructive

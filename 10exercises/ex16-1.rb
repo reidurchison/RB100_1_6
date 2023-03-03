b01a485d056f2c54cb5b27a82df2187caeffcc21
Reid U <rurchison@gmail.com>
2023-03-03 14:23:16 +0000

    3 march 2023 LS progress

--- 10exercises/ex16-1.rb
+++ 10exercises/ex16-1.rb
@@ -1,16 +1,17 @@
-# exercise 16 (challenge)
-# ruby ex16.rb 
+# exercise 16
+# ruby ex16-1.rb
+# read through the solution last night, trying again now to solve it on my own
+
 
 contact_data = ["joe@email.com", "123 Main st.", "555-123-4567"]
 contacts = {"Joe Smith" => {}}
 
-
 symbols = [:email, :address, :phone]
 
-contact_hash = Hash.new
-
-3.times do
-  contact_hash[symbols.shift] = contact_data.shift
+contacts.each do |k, v|
+  symbols.each do |s|
+    v[s] = contact_data.shift
+  end
 end
 
-p contact_hash
+p contacts
